1cd6a5a358c02f541c3b3690cdedbfeb57bb6484
GuidoAugusta <muhammadguido29@students.amikom.ac.id>
2023-10-07 21:48:46 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/field_fox/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.field_fox
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:field_fox/pages/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/login': (context) => const LoginPage(),
+      },
+    );
+  }
+}
